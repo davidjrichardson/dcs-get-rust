ba6640cb63a63e405a3c1ce0a734e16509606864
David Richardson <david@tankski.co.uk>
2016-03-26 14:54:31 +0000

    Add remove parsing

--- src/main.rs
+++ src/main.rs
@@ -93,6 +93,15 @@ Options:
 	-p, --prune-deps  Prune/remove dependencies of the package(s) to be removed
 ";
 
+#[derive(Debug, RustcDecodable)]
+struct RemoveArgs {
+    flag_v: bool,
+    flag_d: bool,
+    flag_p: bool,
+    flag_f: bool,
+    arg_package: Vec<String>,
+}
+
 const USAGE_GENSYMLINKS: &'static str = "
 dcs-get gensymlinks: Generates the symlinks for any new packages, linking them into dcs-get
 
@@ -206,6 +215,13 @@ fn install_command(args: InstallArgs) {
     // TODO
 }
 
+fn remove_command(args: RemoveArgs) {
+    println!("{:?}", args);
+
+    process::exit(0);
+    // TODO
+}
+
 fn parse_args(mut args: Vec<String>) {
     let command = args.remove(0);
 
@@ -289,6 +305,47 @@ fn parse_args(mut args: Vec<String>) {
                     };
 
                     install_command(command_args);
+                }
+                Command::Remove => {
+                    let verbose_index = args.iter()
+                                            .position(|p| p == "-v" || p == "--verbose");
+                    let dry_run_index = args.iter()
+                                            .position(|p| p == "-d" || p == "--dry-run");
+                    let force_index = args.iter()
+                                          .position(|p| p == "-f" || p == "--force");
+                    let prune_index = args.iter()
+                                          .position(|p| p == "-p" || p == "--prune-deps");
+
+                    // Removes all --verbose/--dry-run/etc. flags
+                    let mut packages = args.to_vec();
+                    packages.retain(|p| p != "-v" && p != "--verbose");
+                    packages.retain(|p| p != "-d" && p != "--dry-run");
+                    packages.retain(|p| p != "-f" && p != "--force");
+                    packages.retain(|p| p != "-p" && p != "--prune-deps");
+                    // Necessary copy?
+                    let residual = packages.to_vec();
+
+                    // Check if there are any invalid options left i.e.: --v, -f
+                    let opt_regex = Regex::new(r"^-+\w+").unwrap();
+                    let invalid_args: Vec<_> = residual.iter()
+                                                       .filter(|p| opt_regex.is_match(p))
+                                                       .collect();
+                    // Exit if any invalid args are found
+                    if !invalid_args.is_empty() {
+                        print_help_with_msg(format!("Found invalid aruments: {:?}", invalid_args),
+                                            String::from(USAGE_REMOVE),
+                                            1);
+                    }
+
+                    let command_args = RemoveArgs {
+                        flag_d: dry_run_index.is_some(),
+                        flag_v: verbose_index.is_some(),
+                        flag_f: force_index.is_some(),
+                        flag_p: prune_index.is_some(),
+                        arg_package: packages,
+                    };
+
+                    remove_command(command_args);
                 }
                 _ => println!("{:?}", cmd),
             }
